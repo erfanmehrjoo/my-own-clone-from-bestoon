fd2b326fbfb9526ae8e842e1320f95538adef92d
erfan mehrjoo <90847279+erfanmehrjoo@users.noreply.github.com>
2022-05-21 15:14:34 +0430

    first_initial_commit

---
+++ web/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
